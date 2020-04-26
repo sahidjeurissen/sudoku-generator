0d9f0bb35d4b357b424df2b24272d62232a7787a
Sahid Jeurissen // Partout d.n.a <sahid@partout.nl>
2020-04-26 13:55:31 +0200

    added Stopwatch component for timing generation duration

--- Classes/Commands/GenerateSudokuCommand.php
+++ Classes/Commands/GenerateSudokuCommand.php
@@ -11,6 +11,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Stopwatch\Stopwatch;
 
 class GenerateSudokuCommand extends Command
 {
@@ -19,18 +20,23 @@ class GenerateSudokuCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         global $validSudoku;
+        $stopwatch = new Stopwatch();
 
         $sudokuGenerator = new SudokuGenerator($output);
 
+        $stopwatch->start('generation');
         $sudoku = $sudokuGenerator->generate();
-
-        var_dump(SudokuValidationUtility::validate($sudoku));
+        $event = $stopwatch->stop('generation');
 
         $section = $output->section();
         $table = new Table($section);
         $sudokuRender = array_chunk(array_pad($sudoku, 81, 0), 9);
         $table->setRows($sudokuRender);
         $table->render();
+
+        $section->writeln('Sudoku valid: ' . SudokuValidationUtility::validate($sudoku));
+        $section->writeln('Generation time: ' . $event->getDuration() . 'ms');
+
 
         return 0;
     }
